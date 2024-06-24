1a60805bcf8aceb7036e540b10d8e43798ba83a7
Santino <tinomaturo14@gmail.com>
2024-06-24 11:15:32 -0300

    push

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("searchButton").addEventListener("click", searchMovies);
+
+const buscarPeliculas = () => {
+  let searchInput = document.getElementById("searchInput").value;
+};
